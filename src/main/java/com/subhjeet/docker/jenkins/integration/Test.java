3f1b285ef21dbe8fca03e98450c6e972d5d4de93
subhjeet singh <jeetsmart8@gmail.com>
2024-04-25 12:53:17 +0530

    second commit with sysout

---
+++ src/main/java/com/subhjeet/docker/jenkins/integration/Test.java
@@ -0,0 +1,11 @@
+package com.subhjeet.docker.jenkins.integration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Docker and Jenkins");
+
+	}
+
+}
